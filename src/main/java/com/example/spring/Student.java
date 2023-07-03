6ae719f8810c7270a2c819c3fd2d28cf72ad5157
Sanjeewa Yapa <aquakloud0829@gmail.com>
2023-07-03 15:54:50 +0530

    second commit

--- src/main/java/com/example/spring/Student.java
+++ src/main/java/com/example/spring/Student.java
@@ -1,11 +1,27 @@
 package com.example.spring;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
 public class Student {
+    @Id
     private Long id;
     private String firstName;
     private String lastName;
     private String email;
     private Integer age;
+
+    public Student() {
+    }
+
+    public Student(Long id, String firstName, String lastName, String email, Integer age) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.age = age;
+    }
 
     public Long getId() {
         return id;
